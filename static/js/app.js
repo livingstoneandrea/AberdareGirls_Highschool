f7d12cbf9c28763c4464d753007a26a848e9fe84
livingstoneandrea <livingstoneandrea7@gmail.com>
2021-06-20 20:24:20 +0300

    improved the ui and configured the modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,21 @@
-(function($) {
+(function ($) {
 
     "use strict";
     
-//     var nav_offset_top = $('header').height() + 50;
-//     function navbarFixed() {
-//         if ($('#app-header').length) {
-//             $(window).scroll(function () {
-//                 var scroll = $(window).scrollTop();
-//                 if (scroll >= nav_offset_top) {
-//                     $('#app-header').addClass('navbar_fixed');
-//                 } else {
-//                     $('#app-header').removeClass('navbar_fixed');
-//                 }
-//             });
-//         }
-//     }
-//    navbarFixed();
+    //     var nav_offset_top = $('header').height() + 50;
+    //     function navbarFixed() {
+    //         if ($('#app-header').length) {
+    //             $(window).scroll(function () {
+    //                 var scroll = $(window).scrollTop();
+    //                 if (scroll >= nav_offset_top) {
+    //                     $('#app-header').addClass('navbar_fixed');
+    //                 } else {
+    //                     $('#app-header').removeClass('navbar_fixed');
+    //                 }
+    //             });
+    //         }
+    //     }
+    //    navbarFixed();
 
 
     document.querySelector('#navbar-toggler').addEventListener('click', (e) => {
@@ -29,5 +29,89 @@
     $('.navbar-item').on('click', (e) => {
         $('.navbar-menu, .hamburger').toggleClass('is-active');
     });
+
+    //dynamic modal control
+    const readMoreLinks = document.querySelectorAll('.readmore')
+    var modal = document.getElementById('_modal');
+    let image;
+    let text_info;
+    readMoreLinks.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            let data_extras = e.currentTarget.getAttribute('data-extra_info');
+            console.log(`data extras ${e.currentTarget.getAttribute('data-extra_info')}`);
+            switch (data_extras) {
+                case "about":
+                    image = ""
+                    text_info = "Developers busy "
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+
+                    break;
+                case "learning_progression":
+                    image = ""
+                    text_info = "The content of learning progression is being developed, will notify you soon, "
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+
+                    break;
+                case "event_1":
+                    image = ""
+                    text_info = "The content of event 1 is being developed, will notify you soon"
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+                    break;
+                case "event_2":
+                    image = ""
+                    text_info = "The content of event 2 is being developed, will notify you soon"
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+                    break;
+                case "event_3":
+                    image = ""
+                    text_info = "The content of event 3 is being developed, will notify you soon"
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+                    break;
+                default:
+                   image = ""
+                    text_info = "Oops seemed you clicked on wrong info"
+
+                    document.querySelector("._modal__contents > .wrapper > p").innerHTML = text_info;
+                    if (!modal.classList.contains("is-visible"))
+                        modal.classList.add("is-visible");
+                    
+             }
+        })
+    });
+
+    //close the modal
+    var span = document.getElementsByClassName("_modal-header__close-btn")[0];
+    span.onclick = function() {
+        modal.classList.remove("is-visible")
+        //clear modal data
+        // $('.modal-body').html(' ');
+        // window.location.reload(true);
+
+
+    }
+
+    //  outside click of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+           modal.classList.remove("is-visible")
+        }
+    }
+    
 
 })(jQuery);
